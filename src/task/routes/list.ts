996ef614fc108ba88b2d95c4517d893527812d9f
Logan Chandrasegaran <logan.chandra@ecloudvalley.com>
2022-10-15 10:46:23 +0800

    fix: removed sorting in api

--- src/task/routes/list.ts
+++ src/task/routes/list.ts
@@ -9,16 +9,11 @@ import { TaskInstance } from "../../model/task";
 export const list = async (req: express.Request, res: express.Response) => {
   try {
 
-    let { descDueDate, descCreateDate, search } = req.query
-
-    // CONVERTING STRING TO BOOL
-    let sortDescDueDate = descDueDate == "null" ? null : descDueDate == "true"
-    let sortDescCreateDate = descCreateDate == "null" ? null : descCreateDate == "true"
+    let { search } = req.query
 
     // LIST TASKS
     let tasks = await TaskInstance.findAll({
       where: { name: { [Op.like]: `%${search}%` } },
-      order: defineOrder({ sortDescDueDate, sortDescCreateDate })
     });
 
     // RETURN RESULT
@@ -31,17 +26,3 @@ export const list = async (req: express.Request, res: express.Response) => {
     res.status(500).send({ data: [], msg: "Failed to list tasks" });
   }
 };
-
-const defineOrder = ({ sortDescDueDate, sortDescCreateDate }): OrderItem[] => {
-  let order: OrderItem[] = []
-
-  // IF DueDate NOT NULL PUSH TO ORDER ITEM
-  if (sortDescDueDate != null)
-    order.push(['dueDate', sortDescDueDate ? 'DESC' : 'ASC'])
-
-  // IF CreateDate NOT NULL PUSH TO ORDER ITEM
-  if (sortDescCreateDate != null)
-    order.push(['createDate', sortDescCreateDate ? 'DESC' : 'ASC'])
-
-  return order
-}
